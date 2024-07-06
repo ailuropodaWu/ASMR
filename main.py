4ecd9024006cc2152a92d6a1f29dae4224e9dab5
ailuropodaWu <ailuropoda.cs10@nycu.edu.tw>
2024-07-06 16:42:54 +0800

    fix bug for openai api

--- main.py
+++ main.py
@@ -198,12 +198,12 @@ async def handle_callback(request: Request):
                                 response = openai_client.chat.completions.create(
                                     model="gpt-3.5-turbo",
                                     messages={'role': 'user', 'content': f'請幫我將以下的對話紀錄內容整理成重點\n{chat_history}'}
-                                )
+                                ).choices[0].message
                                 suggest_reply =openai_client.chat.completions.create(
                                     model="gpt-3.5-turbo",
-                                    messages={'role': 'user', 'content': f'我的身分是{user_name}，請幫我產生一句恰當的回覆\n{response.text}'}
-                                )
-                                reply_msg = f'{response.text}\n建議回覆:\n{suggest_reply.text}'
+                                    messages={'role': 'user', 'content': f'我的身分是{user_name}，請幫我產生一句恰當的回覆\n{response}'}
+                                ).choices[0].message
+                                reply_msg = f'{response}\n建議回覆:\n{suggest_reply}'
             else:
                 """
                 Group usage: for getting messages in group
