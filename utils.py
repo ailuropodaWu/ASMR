f688b093f1fbdcd2b7e32767cc73807275d2af6e
ailuropodaWu <ailuropoda.cs10@nycu.edu.tw>
2024-07-06 11:21:17 +0800

    fix bug in utils

--- utils.py
+++ utils.py
@@ -4,5 +4,5 @@ def get_msg_id(fdb: firebase.FirebaseApplication, chat_url):
     msg_ids = fdb.get(chat_url, None)
     if len(msg_ids) == 0:
         return 0
-    msg_ids = msg_ids.key().sort()
+    msg_ids = msg_ids.keys().sort()
     return msg_ids[-1] + 1
