0e087af054c9294e8572bdd47a93cfba7a27dc1f
ailuropodaWu <ailuropoda.cs10@nycu.edu.tw>
2024-07-06 15:53:55 +0800

    add summary of chat

--- main.py
+++ main.py
@@ -32,6 +32,7 @@ from firebase import firebase
 from typing import List
 
 
+ACCOUNT_PATH = 'accounts/'
 
 logging.basicConfig(level=os.getenv('LOG', 'WARNING'))
 logger = logging.getLogger(__file__)
@@ -47,31 +48,23 @@ if channel_access_token is None:
     print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
     sys.exit(1)
 
-configuration = Configuration(
-    access_token=channel_access_token
-)
-
+configuration = Configuration(access_token=channel_access_token)
 api_client = ApiClient(configuration)
 line_bot_api = MessagingApi(api_client)
-
 parser = WebhookParser(channel_secret)
 
 firebase_url = os.getenv('FIREBASE_URL')
+fdb = firebase.FirebaseApplication(firebase_url, None)
+
 gemini_key = os.getenv('GEMINI_API_KEY')
-
-ACCOUNT_PATH = 'accounts/'
-
-# Initialize the Gemini Pro API
 genai.configure(api_key=gemini_key)
 model = genai.GenerativeModel('gemini-1.5-pro')
-
-fdb = firebase.FirebaseApplication(firebase_url, None)
 
 def parse_chat_hsitory(chat_history: List[dict]):
     ret = ''
     for chat in chat_history:
         for sender, text in chat.items():
-            ret += f"{sender}: {text}\n"
+            ret += f"{sender}說{text}\n"
     return ret
 
 def get_all_acounts():
@@ -109,6 +102,9 @@ async def handle_callback(request: Request):
                 Personal usage: to summarize, reply to for the group messagges
                 """
                 if text == '__init__':
+                    """
+                    init the account
+                    """
                     accout_path = ACCOUNT_PATH
                     accounts_list = get_all_acounts()
                     print(accounts_list)
@@ -119,6 +115,9 @@ async def handle_callback(request: Request):
                         fdb.put_async(accout_path, None, accounts_list)
                         reply_msg = "成功啟用"
                 elif text == '__group__':
+                    """
+                    get the group information
+                    """
                     accounts_list = get_all_acounts()
                     if user_id not in accounts_list:
                         reply_msg = "請先啟用"
@@ -131,12 +130,42 @@ async def handle_callback(request: Request):
                             group_name2id = {line_bot_api.get_group_summary(group_id).group_name: group_id for group_id in all_group_data.keys()}
                             reply_msg = '\n'.join(list(group_name2id.keys()))
                 elif text == '__reply__':
+                    """
+                    generate reply for specific group
+                    """
                     accounts_list = get_all_acounts()
                     if user_id not in accounts_list:
                         reply_msg = "請先啟用"
                     else:
                         pass
+                elif text == '__summary__':
+                    accounts_list = get_all_acounts()
+                    at_all = 0
+                    at_person = 0
+                    at_messages = []
+                    if user_id not in accounts_list:
+                        reply_msg = "請先啟用"
+                    else:
+                        chat_store_path = f'chat/{user_id}'
+                        all_group_data = fdb.get(chat_store_path, None)
+                        if all_group_data is None:
+                            reply_msg = '沒有任何群組的資料'
+                        else:
+                            for group_id, chat_history in all_group_data:
+                                group_name = line_bot_api.get_group_summary(group_id).group_name
+                                user_name = line_bot_api.get_group_member_profile(group_id, user_id).display_name
+                                for sender, content in chat_history.items():
+                                    if '@All ' in content:
+                                        at_all += 1
+                                        at_messages.append({sender: content})
+                                    elif f'@{user_name} ' in content:
+                                        at_person += 1
+                                        at_messages.append({sender: content})
+                            reply_msg = f"@ALL: {at_all}次, @YOU: {at_person}次\n{parse_chat_hsitory(at_messages)}"
                 else:
+                    """
+                    mainly handle getting summary of specific group
+                    """
                     accounts_list = get_all_acounts()
                     if user_id not in accounts_list:
                         reply_msg = "請先啟用"
@@ -158,7 +187,7 @@ async def handle_callback(request: Request):
                                 fdb.delete(chat_store_path, group_id)
                                 chat_history = all_group_data[group_id]
                                 chat_history = parse_chat_hsitory(chat_history)
-                                response = model.generate_content(f'請幫我將以下的對話紀錄整理成列表式的重點\n{chat_history}')
+                                response = model.generate_content(f'請幫我將以下的對話紀錄內容整理成重點\n{chat_history}')
                                 reply_msg = response.text
             else:
                 """
