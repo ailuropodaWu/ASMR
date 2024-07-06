989f77dfdbc7a9c2da646b33014e5ae1a4e15e89
ailuropodaWu <ailuropoda.cs10@nycu.edu.tw>
2024-07-06 15:03:47 +0800

    fix bug for accounts

--- main.py
+++ main.py
@@ -114,7 +114,7 @@ async def handle_callback(request: Request):
                     accout_path = ACCOUNT_PATH
                     accounts_list = get_all_acounts()
                     print(accounts_list)
-                    if accout in accounts_list:
+                    if user_id in accounts_list:
                         reply_msg = "已啟用"
                     else:
                         accounts_list.append(user_id)
